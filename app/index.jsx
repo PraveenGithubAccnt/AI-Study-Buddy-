692c72afa32f0b552839e5129dbb779d65e2a6ba
Praveen Kushwaha <praveen2652001kushwaha@gmail.com>
2025-08-01 23:21:01 +0530

    initial file upload

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+   <View className="bg-white p-20 rounded-lg shadow shadow-gray-200 justify-center items-center h-full">
+  <Text className="text-2xl font-bold text-center mb-4 text-red-500">
+    Welcome to Study Buddy!
+  </Text>
+</View>
+  );
+}
